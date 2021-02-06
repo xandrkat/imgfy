64895a51c073b3705b0675f379f4f71e6e3bde30
chipslays <hello@chipslays.ru>
2021-02-06 16:54:58 +0400

    [feat] add header & footer includes

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="mt-4 text-center text-gray-500 text-xs px-4">
+    <div>Imgfy it is forever free & unlimited service for storage images.</div>
+    <div class="">Uploaded images for all time: <strong>{{ $totalImagesCount }}</strong></div>
+    <div class="font-bold mt-2"><a href="/terms" class="text-gray-500 hover:underline mr-1">Terms of Service</a> <a
+            href="/api" class="text-gray-500 hover:underline">API</a></div>
+    <div class="mt-2">&copy; 2020 <a href="//imgfy.cf/" class="text-gray-500 hover:underline font-bold">Imgfy</a></div>
+</footer>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,5 @@
+<div class="w-full h-16 bg-gray-900 bg-opacity-30">
+    <a href="/" class="text-gray-200 h-full font-bold text-xl flex items-center mx-4 uppercase">
+        imgfy
+    </a>
+</div>
